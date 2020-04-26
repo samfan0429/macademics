b2458e7bb369b04e085160b4882498add945c1d4
kyawzazaw <mgkyawzazaw@gmail.com>
2020-04-25 22:19:38 -0500

    Refactor code to scrape using <tbody> hierachy, cleaner now Still need to scrape distrubtion/gened reqs Special thanks to @RohitBagda for guidance

--- scraper/main.py
+++ scraper/main.py
@@ -1,9 +1,7 @@
 from bs4 import BeautifulSoup
 import json
 import re
-import time
 from datetime import *
-
 
 with open("clicked.html", "r") as f:
     contents = f.read()
@@ -13,40 +11,28 @@ bs4_content = BeautifulSoup(contents, "html.parser")
 page_structure = bs4_content.prettify()
 
 headers = ["numsection", "name", "days", "time", "room", "instructor", "availmax"]
-distributions = {"Writing WA", "Writing WP", "Writing WC", "U.S. Identities and Differences", "Internationalism", "Humanities", "Social science", "Fine arts", "Natural science and mathematics", "Quantitative Thinking Q1", "Quantitative Thinking Q2""Quantitative Thinking Q3"}
+distributions = {"Writing WA", "Writing WP", "Writing WC", "U.S. Identities and Differences", "Internationalism",
+                 "Humanities", "Social science", "Fine arts", "Natural science and mathematics",
+                 "Quantitative Thinking Q1", "Quantitative Thinking Q2""Quantitative Thinking Q3"}
 
 
 def row_to_json(content):
-    startTime = datetime.now()
+    dept_tables = content.find_all('tbody')
 
-    # text patterns
-    course_pattern = re.compile("\w{2,4}\s\d{2,3}-[a-zA-Z0-9]{2}")  # regex pattern of any course
-
-    rows = content.find_all('tr')
     sections = []
-    for i in range(len(rows)):
 
-        cells = rows[i].find_all('td')
+    for i in range(0, len(dept_tables)):
+        rows = dept_tables[i].find_all('tr')
+        for j in range(0, len(rows), 2):
+            cells = rows[j].find_all('td')
 
-        # the first <tr> on the page is blank
-        if len(cells) is 0:
-            continue
-
-        first_cell = cells[0].text.strip()
-
-        # if it is a course row, scrap each cell content
-        items = {}
-        if course_pattern.match(first_cell):
-
-            course_id = rows[i]["data-id"]
-
-            # items = {}
+            items = {}
 
             for i in range(len(cells)):
 
                 infopiece = cells[i].text.strip()
 
-                # removing characters like instructor:
+                # removing characters like "instructor:"
                 if i in range(2, 7):
                     infopiece = re.sub(".+:\s", "", infopiece)
 
@@ -54,8 +40,7 @@ def row_to_json(content):
                         infopiece = re.sub("[\w]{6}\s", "", infopiece)
                 items[headers[i]] = infopiece.strip()
 
-            items["courseid"] = course_id
-            # print(items["numsection"])
+            # items["courseid"] = course_id
 
             items["course-num"] = items["numsection"].split('-')[0]
             items["dept"] = items["numsection"].split(' ')[0]
@@ -67,51 +52,46 @@ def row_to_json(content):
             else:
                 items["end"] = "TBA"
 
-            # add each course to the courses list
-            sections.append(items)
-            # dfd
-            courses = {}
-
-            for section in sections:
-                if section['course-num'] not in courses:
-                    courses[section['course-num']] = {
-                        # 'course-num': section['course-num'],
-                        'dept': section['dept'],
-                        'name': section['name'],
-                        'sections': []
-                    }
-
-                courses[section['course-num']]['sections'].append({
-                    'numsection': section['numsection'],
-                    'instructor': section['instructor'],
-                    'days': section['days'],
-                    'availmax': section['availmax'],
-                    'courseid': section['courseid'],
-                    'start': section['start'],
-                    'end': section['end'],
-                    'sectionid': section['courseid']
-
-                    # and so on with the properties you want
-                })
-
-        # details row
-        else:
             # TODO: get <span> content of this issue
             # TODO: somehow parse through all the tags to reach span
             # TODO: Can't use find_all('span) because that way we can't attribute to this specfic course-section
             # TODO: maybe we can use specific course id (these course id also aviablel in <tr data-id="">)
 
-        export_dict = {"fall20": courses}
+            # add each course to the courses list
+            sections.append(items)
+
+    # Section Grouper
+    courses = {}
+    for section in sections:
+        if section['course-num'] not in courses:
+            courses[section['course-num']] = {
+                # 'course-num': section['course-num'],
+                'dept': section['dept'],
+                'name': section['name'],
+                'sections': []
+            }
+
+        courses[section['course-num']]['sections'].append({
+            'numsection': section['numsection'],
+            'instructor': section['instructor'],
+            'days': section['days'],
+            'availmax': section['availmax'],
+            'start': section['start'],
+            'end': section['end'],
+            # 'sectionid': section['courseid']
+        })
 
     # export as JSON
+    export_dict = {"fall20": courses}
+
     with open('fall20.json', 'w') as fout:
         json.dump(export_dict, fout, indent=4)
 
-    print('\nTime elasped: ', datetime.now() - startTime)
-
 
 def main():
-    row_to_json(content)
+    startTime = datetime.now()
+    row_to_json(bs4_content)
+    print('\nTime elasped: ', datetime.now() - startTime)
 
 
 if __name__ == '__main__':
